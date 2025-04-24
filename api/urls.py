5fcd0214fd189ebd50b36c3a90a184d5f7d3a74d
Govind <govindkrishna1102@gmail.com>
2025-04-24 21:44:21 +0530

    Final

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from api import views
 
 urlpatterns=[
-    
+    path('lead/',views.LeadView.as_view()),
+    path('lead/<int:pk>/',views.LeadRetrieveUpdateDestroyView.as_view())
 ]
